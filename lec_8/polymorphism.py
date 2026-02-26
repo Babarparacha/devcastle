393939cd399efe72a40ca2d33dd362a634aff86b
babarParacha <babarmuzaffar3@gmail.com>
2026-02-26 11:16:55 +0500

    fix spellling mistake

--- lec_8/polymorphism.py
+++ lec_8/polymorphism.py
@@ -1,14 +1,14 @@
 class Model:
     def train(self):
         print("training using basic method")
-class neuralNetwork(Model):
+class NeuralNetwork(Model):
     def train(self):
         print("training using backpropagation")
 class LinearRegression(Model):
     def train(self):
         print("training using least  square")
 
-model=[neuralNetwork(),LinearRegression()]
+model=[NeuralNetwork(),LinearRegression()]
 # print(type(model[0]))
 for m in model:
     m.train()
